523a08b409fca95c9e60ea904c9cc25ddbbbfb42
tonyntran <tonytran11498@gmail.com>
2026-09-01 20:43:24 -0700

    feat: add blog_post content type and image storage bucket

---
+++ supabase/migrations/20260901000000_blog_posts.sql
@@ -0,0 +1,19 @@
+-- Allow blog_post as a content_blocks type
+ALTER TABLE content_blocks DROP CONSTRAINT chk_content_blocks_type;
+ALTER TABLE content_blocks ADD CONSTRAINT chk_content_blocks_type
+  CHECK (type IN ('hero', 'about', 'experience', 'skill', 'project', 'contact', 'blog_post'));
+
+-- Enforce unique slugs among blog posts
+CREATE UNIQUE INDEX idx_content_blocks_blog_slug
+  ON content_blocks ((metadata->>'slug'))
+  WHERE type = 'blog_post';
+
+-- Public bucket for blog cover/inline images
+INSERT INTO storage.buckets (id, name, public)
+VALUES ('blog-images', 'blog-images', true);
+
+CREATE POLICY "public_read_blog_images" ON storage.objects
+  FOR SELECT TO anon USING (bucket_id = 'blog-images');
+
+CREATE POLICY "auth_write_blog_images" ON storage.objects
+  FOR ALL TO authenticated USING (bucket_id = 'blog-images') WITH CHECK (bucket_id = 'blog-images');
